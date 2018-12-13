4f5b6a81e5d2dee4523e6b46ad8719733745de84
zaheer <m.abbaszaheer@gmail.com>
2018-12-13 11:40:02 -0500

    squaring the number each method

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
+#Square the elements of input array "array" and store the square of numbers in the new array "square_arr".
+
 def square_array(array)
-  # your code here
+  square_arr= []
+  array.each { |element| square_arr.push(element ** 2) }
+  square_arr
 end
+
+=begin
+
+This does not pass the test, but i believe it is the correct.
+
+ def square_array(array)
+    square_arr=array.map{|x| x ** 2}
+  end
+
+=end
+
+
